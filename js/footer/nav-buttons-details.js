f398cf44863bd251a95a3bdc486ca545e7968a2d
Paweł Bednarczyk <bedpaw97@gmail.com>
2020-06-09 23:50:02 +0200

    Create new files and organize project

---
+++ js/footer/nav-buttons-details.js
@@ -0,0 +1,12 @@
+export const navButtons = [
+  {
+    passiveText: 'O co chodzi',
+    activeText: '',
+    passiveClass: 'about-event',
+    activeClass: 'Kontakt'
+<button class="nav-btn about-event">O co chodzi</button>
+<button class="nav-btn how-to-help">Jak pomóc?</button>
+<button class="nav-btn event-aim">Cel</button>
+  <button class="nav-btn contact">Kontakt</button>
+},
+]
